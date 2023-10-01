fb6ca8510321cce4d0fe7dc70bde9cf8cdfdfc83
Christos Papaloukas <cpapaloukas@skroutz.gr>
2023-10-01 22:07:07 +0300

    Downcase `Ingredient` name and add case-insensitive uniqueness

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,13 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :recipes, through: :recipe_ingredients
 
-  validates :name, uniqueness: true, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  before_save :downcase_name, if: -> { name_changed? }
+
+  private
+
+  def downcase_name
+    self.name = name.downcase
+  end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe Ingredient, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+
+    it 'downcases name before save' do
+      ingredient = described_class.create(name: 'Milk')
+      expect(ingredient.name).to eq('milk')
+    end
   end
 end
