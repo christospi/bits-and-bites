7f678aa195b2b042a8e57296c28c35b8137040b7
Christos Papaloukas <cpapaloukas@skroutz.gr>
2023-10-04 14:58:23 +0300

    Support searching by name on `Ingredient` model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,10 +1,14 @@
 class Ingredient < ApplicationRecord
+  include PgSearch::Model
+
   has_many :recipes_ingredients, dependent: :destroy
   has_many :recipes, through: :recipes_ingredients
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   before_save :downcase_name, if: -> { name_changed? }
+
+  pg_search_scope :search_by_name, against: :name
 
   private
 
