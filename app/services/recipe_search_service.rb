2fb54c2b25d7ec1332d47dd1d57c86526cc90b5e
Christos Papaloukas <cpapaloukas@skroutz.gr>
2023-10-04 14:58:55 +0300

    Support full-text ingredients search

--- app/services/recipe_search_service.rb
+++ app/services/recipe_search_service.rb
@@ -13,14 +13,21 @@ class RecipeSearchService
   def call
     return Recipe.none if @ingredients.blank?
 
+    # TODO: We should cache most-searched ingredients, once we store the
+    #   searches somewhere.
+    # TODO: We should handle common typos, stems, synonyms, etc... someday :)
+    matched_ingredient_ids = @ingredients.flat_map do |ingredient_name|
+      Ingredient.search_by_name(ingredient_name).pluck(:id)
+    end.uniq
+
     # Get the recipes that have ingredients other than the specified ones.
     # Notes:
     # (1) We use RecipesIngredient directly to avoid an extra query, since
-    # we only need the recipe_id and the ingredient name.
+    # we only need the recipe_id and the ingredient_id.
     # (2) Also, we do not materialize the ActiveRecord objects to let the DB do
-    # the heavy lifting with nested query.
-    not_matching_recipes = RecipesIngredient.joins(:ingredient).
-      where.not(ingredients: { name: @ingredients }).
+    # the heavy lifting with a nested query.
+    not_matching_recipes = RecipesIngredient.
+      where.not(ingredient_id: matched_ingredient_ids).
       distinct.select('recipe_id')
 
     # The recipes that all their ingredients are included in the specified ones.
